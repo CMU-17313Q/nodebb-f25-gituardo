97ca8fdff3c417cee5ca0f0d0bee8601c54c4cce
Haneen <hkhuzaei@andrew.cmu.edu>
2025-10-04 15:57:46 +0000

    added comments in the reactions.js controllers file

--- src/controllers/reactions.js
+++ src/controllers/reactions.js
@@ -3,17 +3,22 @@
 const db = require('../database');
 const posts = require('../posts');
 
+//object to store all reaction related functions
 const Reactions = {};
 
+//function to add or remove a user's reaction on a post
 Reactions.toggle = async function (req, res, next) {
 	try {
+		//get the user ID from the request and the post ID and reaction type from the body
 		const { uid } = req;
 		const { pid, type } = req.body;
 		
+		//any required data is missing, return a 400 error
 		if (!uid || !pid || !type) {
 			return res.status(400).json({ error: 'missing-data' });
 		}
 		
+		//post doesn’t exist, return a 404 error
 		const post = await posts.getPostData(parseInt(pid, 10));
 		if (!post) {
 			return res.status(404).json({ error: 'no-post' });
@@ -24,6 +29,7 @@ Reactions.toggle = async function (req, res, next) {
 		
 		const already = await db.isSetMember(reactedKey, uid);
 		
+		//if the user already reacted, remove their reaction
 		if (already) {
 			await Promise.all([
 				db.setRemove(reactedKey, uid),
@@ -36,8 +42,10 @@ Reactions.toggle = async function (req, res, next) {
 			]);
 		}
 		
+		//count how many users have reacted with this type after the update
 		const count = await db.setCount(reactedKey);
 		
+		//the updated reaction info as a json response
 		res.json({
 			pid: parseInt(pid, 10),
 			type,
